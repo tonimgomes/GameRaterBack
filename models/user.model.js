17469d5d1903cfdec3137e4db5ff05abebd374f7
tonimgomes <antoniosames07@gmail.com>
2023-07-04 13:39:13 -0300

    Removendo erro do bcryptjs

--- models/user.model.js
+++ models/user.model.js
@@ -1,47 +1,3 @@
-const bcrypt = require('bcryptjs');
-const SALT_ROUNDS = 10;
-
-module.exports = (mongoose) => {
-  let schema = mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    email: {
-      type: String,
-      required: true,
-      trim: true,
-      unique: true,
-      lowercase: true
-    },
-    password: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    __v: {
-      type: Number,
-      select: false
-    }
-  });
-
-  schema.statics.generateHash = async function (password) {
-    const salt = await bcrypt.genSalt(SALT_ROUNDS);
-    return bcrypt.hash(password, salt);
-  };
-
-  schema.methods.validatePassword = async function (password) {
-    return bcrypt.compare(password, this.password);
-  };
-
-  const User = mongoose.model('users', schema);
-
-  return User;
-};
-
-
-/*
 const bcrypt = require('bcrypt');
 const SALT_ROUNDS = 10;
 
@@ -82,4 +38,4 @@ module.exports = (mongoose) => {
     const User = mongoose.model('users', schema);
 
     return User;
-};*/
+}
